0877686942a9fdc7ba102133d3e7dcf13fcd3538
Yumokaro <43580935+Yumokaro@users.noreply.github.com>
2019-03-10 11:57:13 -0700

    Exercise 16

---
+++ Exercise 16.py
@@ -0,0 +1,32 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"we're going to erase {filename}.")
+print("if you don't want that, hit 'ctrl-c'.")
+print("if you do want that, hit 'return'.")
+
+input("?")
+
+print("opening file...")
+target = open(filename, 'w')
+
+print("truncating the file. bye!")
+target.truncate()
+
+print("now i'm going to ask you for three lines.")
+
+line1 = input("line1: ")
+line2 = input("line2: ")
+line3 = input("line3: ")
+
+print("i'm going to write these lines to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+
+print("and finally, we close it.")
+target.close()
