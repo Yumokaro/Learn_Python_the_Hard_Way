aef2d023a31386d663a50befadbae48e482fea3c
Yumokaro <43580935+Yumokaro@users.noreply.github.com>
2019-03-29 14:49:36 -0700

    Exercise 29

---
+++ Exercise 29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("too many cats here.  We're doomed")
+
+if people > cats:
+    print("not too many cats! we're fine")
+
+if people < dogs:
+    print("too many dogs.")
+
+if people > dogs:
+    print("more people than dogs")
+
+dogs += 5
+
+if people >= dogs:
+    print("just as many people as there are dogs")
+
+if people <= dogs:
+    print("less or equal people to dogs")
+
+if people == dogs:
+    print("people equals dogs")
